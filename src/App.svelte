acf0577d37b8b6da8de7394d1ea27f11c987c4b5
Kirill Vasiltsov <v_kirill@yumemi.co.jp>
2020-07-09 19:24:44 +0900

    Initial

---
+++ src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+  export let name;
+</script>
+
+<style>
+
+</style>
+
+<main>
+  <h1>Reasons why React components render</h1>
+  <section>
+    <h2>Parent component rendered</h2>
+    <iframe
+      src="https://codesandbox.io/embed/simple-rendering-example-4jou1?codemirror=1&fontsize=14&hidenavigation=1&module=%2Fsrc%2FApp.js&theme=dark&view=preview&hidedevtools=1&
+      hidenavigation=1"
+      style="width:100%; height:500px; border:0; border-radius: 4px;
+      overflow:hidden;"
+      title="Simple rendering example"
+      allow="accelerometer; ambient-light-sensor; camera; encrypted-media;
+      geolocation; gyroscope; hid; microphone; midi; payment; usb; vr;
+      xr-spatial-tracking"
+      sandbox="allow-autoplay allow-forms allow-modals allow-popups
+      allow-presentation allow-same-origin allow-scripts" />
+  </section>
+  <section>
+    <h2>Parent component rendered</h2>
+    <iframe
+      src="https://codesandbox.io/embed/simple-rendering-example-4jou1?codemirror=1&fontsize=14&hidenavigation=1&module=%2Fsrc%2FApp.js&theme=dark&view=preview&hidedevtools=1&
+      hidenavigation=1"
+      style="width:100%; height:500px; border:0; border-radius: 4px;
+      overflow:hidden;"
+      title="Simple rendering example"
+      allow="accelerometer; ambient-light-sensor; camera; encrypted-media;
+      geolocation; gyroscope; hid; microphone; midi; payment; usb; vr;
+      xr-spatial-tracking"
+      sandbox="allow-autoplay allow-forms allow-modals allow-popups
+      allow-presentation allow-same-origin allow-scripts" />
+  </section>
+</main>
